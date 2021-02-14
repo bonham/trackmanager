338ee75e11a7265bcf55606f2bc23673b84ad335
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-02-15 00:42:15 +0100

    draft frontend backend handshake
    with cors error

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {
+      id: 1,
+      name: "Peter"
+    },
+    {
+      id: 2,
+      name: "Paul"
+    }
+  ])
 });
 
 module.exports = router;

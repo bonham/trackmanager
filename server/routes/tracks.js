5160cc0e04f5086dce56eb26dc3b76d183add61d
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-05-22 19:05:57 +0200

    return geojson

--- server/routes/tracks.js
+++ server/routes/tracks.js
@@ -4,6 +4,7 @@ const { Client } = require('pg')
 const logger = require('morgan');
 
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
@@ -12,6 +13,7 @@ router.get('/', function(req, res, next) {
     host: 'localhost',
     database: 'gpx1',
   })
+  
 
   client.connect()
     .then(() => { return client.query("select id, name, length, src, time, timelength, ascent from tracks")})
@@ -27,22 +29,29 @@ router.get('/', function(req, res, next) {
 
 router.get('/:trackId', function(req, res, next) {
 
-  // trackId = req.params["trackId"]
+  const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'gpx1',
+  })
+  
+  trackId = req.params["trackId"]
 
-  // query = "select ST_AsGeoJSON(wkb_geometry) from tracks where id = '"+trackId+"'"
-  // logger(query)
+  query = "select ST_AsGeoJSON(wkb_geometry) from tracks where id = '"+trackId+"'"
+  logger(query)
 
-  // client.connect()
-  // .then(() => { return client.query(query)})
-  // .then((result) => {
-  //   res.json(result.rows)
-  // })
-  // .catch(err => {
-  //   console.log(err)
-  //   res.status(500).send(err.message)
-  // })
-  rres.send("empty")
-
+  client.connect()
+  .then(() => { return client.query(query)})
+  .then((result) => {
+    jsonString = result.rows[0]["st_asgeojson"]
+    geoJson = JSON.parse(jsonString)
+    res.json(geoJson)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).send(err.message)
+  })
+  
   
 });
 

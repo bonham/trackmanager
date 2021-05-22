33a577e8c676c4a96066e94f4bb6533c2f26578b
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-05-22 18:56:28 +0200

    logging

--- server/routes/tracks.js
+++ server/routes/tracks.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { Client } = require('pg')
+const logger = require('morgan');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -25,9 +27,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/:trackId', function(req, res, next) {
 
-    r = {}
-    r.id = req.params["trackId"]
-    res.json(r)
+  // trackId = req.params["trackId"]
+
+  // query = "select ST_AsGeoJSON(wkb_geometry) from tracks where id = '"+trackId+"'"
+  // logger(query)
+
+  // client.connect()
+  // .then(() => { return client.query(query)})
+  // .then((result) => {
+  //   res.json(result.rows)
+  // })
+  // .catch(err => {
+  //   console.log(err)
+  //   res.status(500).send(err.message)
+  // })
+  rres.send("empty")
+
   
 });
 

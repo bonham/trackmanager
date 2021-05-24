d0c9ff566ba8b2e883e94b60e935c066c6c62125
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-05-24 21:38:19 +0200

    zoom to extent

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -7,13 +7,14 @@
 
 <script>
 import { Map, View } from 'ol'
+import { transformExtent } from 'ol/proj'
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
+import Track from '@/lib/Track.js'
 
 function initMap () {
 // eslint-disable-next-line no-unused-vars
   const map = new Map({
-    target: 'mapdiv',
     layers: [
       new TileLayer({
         source: new OSM()
@@ -25,6 +26,35 @@ function initMap () {
     })
   })
   this.map = map
+
+  getTrack(this.trackId, map)
+}
+
+function setTarget () {
+  this.map.setTarget('mapdiv')
+}
+
+// function zoomBoundingBox() {
+
+// }
+
+function getTrack (tid, map) {
+  const url = '/api/tracks/' + tid
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const track = new Track(data)
+      const extent = transformExtent(
+        track.geojson.bbox,
+        'EPSG:4326',
+        'EPSG:3857'
+      )
+
+      map.getView().fit(
+        extent,
+        map.getSize()
+      )
+    })
 }
 
 export default {
@@ -40,7 +70,8 @@ export default {
       map: null
     }
   },
-  mounted: initMap
+  created: initMap,
+  mounted: setTarget
 }
 
 </script>

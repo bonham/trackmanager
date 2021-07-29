1c91ffc9e033ac1624407ae61d276e75224c0675
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-07-30 00:59:18 +0200

    add icons

--- client/src/components/TrackCard.vue
+++ client/src/components/TrackCard.vue
@@ -1,20 +1,34 @@
 <template>
-  <b-link
-    :to="'/track/'+track.id"
-    router-tag="div"
+  <b-card
+    class="my-2"
   >
-    <b-card
-      :title="track.src"
-      class="my-2"
-    >
-      <b-card-text>
-        {{ track.monthAndDay() }}
-        / {{ (track.distance() / 1000).toFixed(2) }} km
-        / {{ Math.round(track.ascent) }} m
-        / {{ track.timeLengthFormatted() }} h
-      </b-card-text>
-    </b-card>
-  </b-link>
+    <b-card-text>
+      <b-row class="align-items-center">
+        <b-col cols="8">
+          <h4>
+            {{ track.src }}
+          </h4>
+          <div>
+            {{ track.monthAndDay() }}
+            / {{ (track.distance() / 1000).toFixed(2) }} km
+            / {{ Math.round(track.ascent) }} m
+            / {{ track.timeLengthFormatted() }} h
+          </div>
+        </b-col>
+        <b-col
+          cols="4"
+          class="text-end"
+        >
+          <b-icon-check-square class="mx-4 card-icon" />
+          <b-link
+            class="mx-4 card-icon"
+            :to="'/track/'+track.id"
+            router-tag="b-icon-zoom-in"
+          />
+        </b-col>
+      </b-row>
+    </b-card-text>
+  </b-card>
 </template>
 
 <script>
@@ -31,3 +45,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.card-icon {
+  font-size: 3rem;
+}
+</style>

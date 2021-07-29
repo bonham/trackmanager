8ddba09846e7f126d16bc6860d7cad4ff5df978b
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-07-29 23:46:41 +0200

    remove superfluous div

--- client/src/components/TrackSection.vue
+++ client/src/components/TrackSection.vue
@@ -1,44 +1,39 @@
 <template>
-  <div>
-    <!-- <TrackHeader /> -->
-    <b-row>
-      <b-col>
-        <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto">
-          <b-card
-            bg-variant="light"
-            :title="label"
-            class="my-2"
-            @click="toggleMemberVisibility"
+  <b-row>
+    <b-col class="px-3 py-3 pt-md-5 pb-md-4 mx-auto">
+      <b-card
+        bg-variant="light"
+        :title="label"
+        class="my-2"
+        @click="toggleMemberVisibility"
+      >
+        <b-card-text>
+          <b-button
+            :class="expanded ? null : 'collapsed'"
+            :aria-expanded="expanded ? 'true' : 'false'"
+            :aria-controls="collapseId"
           >
-            <b-card-text>
-              <b-button
-                :class="expanded ? null : 'collapsed'"
-                :aria-expanded="expanded ? 'true' : 'false'"
-                :aria-controls="collapseId"
-              >
-                <b-icon
-                  :icon="expandIcon"
-                />
-              </b-button>
-              <span class="mx-2">
-                {{ Math.round(coll.distance() / 1000) }} km total
-              </span>
-            </b-card-text>
-          </b-card>
-          <b-collapse
-            :id="collapseId"
-            v-model="expanded"
-          >
-            <TrackCard
-              v-for="item in myDataList"
-              :key="item.id"
-              :track="item"
+            <b-icon
+              :icon="expandIcon"
             />
-          </b-collapse>
-        </div>
-      </b-col>
-    </b-row>
-  </div>
+          </b-button>
+          <span class="mx-2">
+            {{ Math.round(coll.distance() / 1000) }} km total
+          </span>
+        </b-card-text>
+      </b-card>
+      <b-collapse
+        :id="collapseId"
+        v-model="expanded"
+      >
+        <TrackCard
+          v-for="item in myDataList"
+          :key="item.id"
+          :track="item"
+        />
+      </b-collapse>
+    </b-col>
+  </b-row>
 </template>
 
 <script>

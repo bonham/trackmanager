8be788245a27dcecb0be8ad987cf9c1bd4c9641e
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-08-24 09:41:37 +0200

    clean up main imports

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,13 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue'
-// import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
 
 // import { BootstrapVue } from 'bootstrap-vue'
 import { LayoutPlugin, NavbarPlugin, ButtonPlugin, LinkPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
+import router from './router'
+
 Vue.use(LayoutPlugin)
 Vue.use(NavbarPlugin)
 Vue.use(ButtonPlugin)

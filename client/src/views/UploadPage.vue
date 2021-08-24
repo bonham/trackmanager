cb2eae44d6aba9f3c442dec263c97225ff1c5387
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-08-24 10:27:32 +0200

    develop upload

--- client/src/views/UploadPage.vue
+++ client/src/views/UploadPage.vue
@@ -1,27 +1,57 @@
 <template>
   <div>
-    <b-form-file
-      v-model="file1"
-      :state="Boolean(file1)"
-      placeholder="Choose a file or drop it here..."
-      drop-placeholder="Drop file here..."
-    />
-    <div class="mt-3">
-      Selected file: {{ file1 ? file1.name : '' }}
-    </div>
+    <b-form
+      v-if="show"
+      @submit="onSubmit"
+      @reset="onReset"
+    >
+      <b-form-file
+        v-model="file1"
+        :state="Boolean(file1)"
+        placeholder="Choose a file or drop it here..."
+        drop-placeholder="Drop file here..."
+      />
+      <div class="mt-3">
+        Selected file: {{ file1 ? file1.name : '' }}
+      </div>
+      <b-button
+        type="submit"
+        variant="primary"
+      >
+        Submit
+      </b-button>
+      <b-button
+        type="reset"
+        variant="danger"
+      >
+        Reset
+      </b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-import { BFormFile } from 'bootstrap-vue'
+import { BForm, BFormFile } from 'bootstrap-vue'
 export default {
   name: 'UploadPage',
   components: {
+    BForm,
     BFormFile
   },
   data () {
     return {
-      file1: null
+      file1: null,
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      alert(`submit ${this.file1.name}`)
+    },
+    onReset (event) {
+      event.preventDefault()
+      this.file1 = null
     }
   }
 }

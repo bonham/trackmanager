c9fb5721993d6dc59026bcd2f69b16ea419b4768
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-09-13 00:30:50 +0200

    prototype for edit table

--- client/src/views/TrackMultiEdit.vue
+++ client/src/views/TrackMultiEdit.vue
@@ -3,10 +3,35 @@
     <h1 class="mt-4 mb-4">
       Edit Tracks
     </h1>
+    <b-table
+      striped
+      hover
+      :items="trackFlatList"
+    />
   </div>
 </template>
 
 <script>
+import { BTable } from 'bootstrap-vue'
+
 export default {
+  name: 'TrackMultiEdit',
+  components: {
+    BTable
+  },
+  data () {
+    return {
+      trackFlatList: []
+    }
+  },
+  created: function () {
+    fetch('/api/tracks')
+      .then(response => response.json())
+      .then(data => {
+        this.trackFlatList = data.sort((a, b) => { return (a.id - b.id) })
+        return data
+      }, err => console.log(err))
+  }
+
 }
 </script>

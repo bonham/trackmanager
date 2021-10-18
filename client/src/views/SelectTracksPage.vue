c351d7d077644078b6b831f541c06b41b5a7da96
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-10-18 22:00:37 +0200

    split slider first version

--- client/src/views/SelectTracksPage.vue
+++ client/src/views/SelectTracksPage.vue
@@ -1,19 +1,27 @@
 <template>
   <div
     id="root"
-    class="position-absolute h-100 w-100 border border-primary d-flex flex-column"
+    class="position-absolute h-100 w-100 d-flex flex-column"
   >
     <track-manager-nav-bar />
-    <div class="d-flex flex-row border border-warning flex-grow-1">
+    <div class="d-flex flex-row flex-grow-1">
       <div
-        id="one"
-        class="flex-grow-1 border border-primary overflow-hidden"
+        id="left"
+        ref="left"
+        class="overflow-hidden"
+        :style="leftBoxStyleObject"
       >
         left
       </div>
       <div
-        id="two"
-        class="flex-grow-1 border border-primary overflow-hidden"
+        id="middle"
+        ref="middle"
+        class="overflow-hidden"
+        @mousedown="dragMouseDown"
+      />
+      <div
+        id="right"
+        class="flex-grow-1 overflow-hidden"
       >
         right
       </div>
@@ -33,11 +41,42 @@ export default {
     return {
       text: `
           Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        `
+        `,
+      positions: {
+        clientX: undefined,
+        clientY: undefined,
+        movementX: 0,
+        movementY: 0
+      },
+      leftBoxStyleObject: {
+        'flex-basis': '60px'
+      }
+    }
+  },
+  methods: {
+    dragMouseDown: function (event) {
+      event.preventDefault()
+      this.positions.clientX = event.clientX
+      document.onmousemove = this.borderDrag
+      document.onmouseup = this.closeDragElement
+    },
+    borderDrag: function (event) {
+      event.preventDefault()
+      this.positions.movementX = this.positions.clientX - event.clientX
+      this.positions.clientX = event.clientX
+      this.leftBoxStyleObject['flex-basis'] = (this.$refs.left.offsetWidth - this.positions.movementX) + 'px'
+    },
+    closeDragElement: function () {
+      document.onmouseup = null
+      document.onmousemove = null
     }
   }
 }
 </script>
 <style scoped>
 
+#middle {
+  background-color: lightgrey;
+  flex-basis: 10px;
+}
 </style>

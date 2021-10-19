96e96cd04b59d02d31b4346112b0a4beaf65a6c3
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-10-19 21:44:45 +0200

    add container

--- client/src/views/TrackOverviewPage.vue
+++ client/src/views/TrackOverviewPage.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
+  <b-container>
     <track-manager-nav-bar />
     <h1 class="mt-4 mb-4">
       Track Overview
     </h1>
     <TrackOverview />
-  </div>
+  </b-container>
 </template>
 
 <script>
+import { BContainer } from 'bootstrap-vue'
 import TrackManagerNavBar from '@/components/TrackManagerNavBar.vue'
 export default {
   name: 'TrackOverviewPage',
   components: {
     TrackOverview: () => import(/* webpackChunkName: "TrackOverView" */ '@/components/TrackOverview.vue'),
-    TrackManagerNavBar
+    TrackManagerNavBar,
+    BContainer
   }
 }
 </script>

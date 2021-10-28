3f5b0e60f4de9cd8b274bf992447542c6a7000c4
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-10-28 21:49:06 +0200

    add map in splitview

--- client/src/components/FilteredMap.vue
+++ client/src/components/FilteredMap.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    {{ testprop }}
+  <div class="position-absolute">
+    <map-component />
   </div>
 </template>
 <script>
+import MapComponent from '@/components/MapComponent.vue'
 export default {
   name: 'FilteredMap',
+  components: { MapComponent },
   data () {
     return {
       testprop: 'nix2'

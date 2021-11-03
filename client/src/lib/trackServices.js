a08d9e143a1d1ecaabba4c5d7b79b5c305b90dc9
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-11-03 19:56:11 +0100

    add getGeoJson

--- client/src/lib/trackServices.js
+++ client/src/lib/trackServices.js
@@ -1,14 +1,43 @@
 import { Track } from '@/lib/Track.js'
 const _ = require('lodash')
 
+// /// Get all tracks
 async function getAllTracks () {
-  const response = await fetch('/api/tracks')
+  const response = await fetch('/api/tracks/getall')
   const responseJson = await response.json()
 
   const trackArray = responseJson.map(t => new Track(t))
   return trackArray
 }
 
+// /// Get geojson by id
+async function getGeoJson (idList) {
+  const payload = { ids: idList }
+
+  const url = '/api/tracks/geojson/'
+  const req = new Request(
+    url,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    }
+
+  )
+
+  const response = await fetch(req)
+  if (response.ok) {
+    const respJson = response.json()
+    return respJson
+  } else {
+    const errText = await response.text()
+    throw new Error(`Response code ${response.status} after fetching to ${url}, error: ${errText}`)
+  }
+}
+
+// /// Update Track
 async function updateTrack (track, attributes) {
   const id = track.id
   const data = _.pick(track, attributes)
@@ -46,4 +75,4 @@ async function updateTrack (track, attributes) {
   }
 }
 
-export { getAllTracks, updateTrack }
+export { getAllTracks, getGeoJson, updateTrack }

61fe4ff2e21a9fd5149cb84875aa7bad536ca200
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-11-14 22:32:00 +0100

    test selecttracks

--- client/tests/unit/selectTracks.test.js
+++ client/tests/unit/selectTracks.test.js
@@ -7,24 +7,38 @@ const localVue = createLocalVue()
 
 localVue.use(Vuex)
 
+function mountAndRunTest (testFunction) {
+  return import('../../src/store.js').then((module) => {
+    const store = module.default
+    const elem = document.createElement('div')
+    if (document.body) {
+      document.body.appendChild(elem)
+    }
+
+    const wrapper = shallowMount(SelectTracksPage, { store, localVue, attachTo: elem })
+    testFunction(wrapper)
+    wrapper.destroy()
+  })
+}
+
 describe('SelectTracksPage', () => {
+  beforeEach(() => {
+    // mock window.matchMedia
+    const mockMatchMedia = jest.fn(() => { return { matches: false } })
+    global.window.matchMedia = mockMatchMedia
+  })
   afterEach(() => {
     jest.restoreAllMocks()
   })
-  test('Trivial mount', () => {
-    return import('../../src/store.js').then((module) => {
-      const store = module.default
-      const elem = document.createElement('div')
-      if (document.body) {
-        document.body.appendChild(elem)
-      }
-
-      // mock window.matchMedia
-      const mockMatchMedia = jest.fn(() => { return { matches: false } })
-      global.window.matchMedia = mockMatchMedia
-
-      const wrapper = shallowMount(SelectTracksPage, { store, localVue, attachTo: elem })
+  test('Initial layout', () => {
+    mountAndRunTest((wrapper) => {
       expect(wrapper.vm.currentLayout).toEqual('landscape')
+    })
+  })
+  test('Layout after toggle', () => {
+    mountAndRunTest((wrapper) => {
+      wrapper.vm.toggleLayout()
+      expect(wrapper.vm.currentLayout).toEqual('portrait')
     })
   })
 })

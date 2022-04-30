f78e22b90bf8e43d36c32fe98ef93492e6b56b25
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-04-30 17:01:00 +0200

    include navbar in detail page

--- client/src/views/TrackDetailPage.vue
+++ client/src/views/TrackDetailPage.vue
@@ -3,8 +3,11 @@
     id="root"
     class="d-flex flex-column vh-100"
   >
+    <track-manager-nav-bar :sid="sid" />
     <div class="flex-grow-1 d-flex flex-column">
-      <h1>Track {{ $route.params.id }} Details</h1>
+      <h1 class="mt-4 mb-4">
+        Track {{ $route.params.id }} Details
+      </h1>
       <MapComponent
         :track-id="Number.parseInt($route.params.id)"
         :sid="sid"
@@ -16,12 +19,14 @@
 <script>
 import MapComponent from '@/components/MapComponent.vue'
 import { BContainer } from 'bootstrap-vue'
+import TrackManagerNavBar from '@/components/TrackManagerNavBar.vue'
 
 export default {
   name: 'MapC',
   components: {
     MapComponent,
-    BContainer
+    BContainer,
+    TrackManagerNavBar
   },
   props: {
     sid: {

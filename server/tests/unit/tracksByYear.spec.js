a0f97d7a4ed70cdbf854bc9a752aa7431980351e
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-05-07 23:26:19 +0200

    add test for updatetrackbyid
    make tests more flexible

--- server/tests/unit/tracksByYear.spec.js
+++ server/tests/unit/tracksByYear.spec.js
@@ -1,9 +1,16 @@
 const request = require('supertest')
 const app = require('../../app')
-jest.mock('pg')
-const { Pool } = require('pg')
 jest.mock('../../lib/getSchema')
 const mockGetSchema = require('../../lib/getSchema')
+const { Pool } = require('pg')
+jest.mock('pg', () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn()
+  }
+  return { Pool: jest.fn(() => mClient) }
+})
 
 const mockTrack1 = {
   id: '2',
@@ -14,18 +21,17 @@ const mockTrack1 = {
   ascent: 3
 }
 
-// eslint-disable-next-line no-unused-vars
-const queryMock = jest
-  .spyOn(Pool.prototype, 'query')
-  .mockResolvedValue({
-    rows: [mockTrack1]
-  })
-
 describe('tracks - byYear', () => {
-  beforeEach(
+  let mockPool
+  beforeEach(() => {
     mockGetSchema.mockReset()
-  )
+    mockPool = new Pool()
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
   test('happy path', async () => {
+    mockPool.query.mockResolvedValue({ rows: [mockTrack1] })
     mockGetSchema.mockResolvedValue('myschema')
     const response = await request(app)
       .get('/api/tracks/byyear/2021/sid/correct')
@@ -34,12 +40,11 @@ describe('tracks - byYear', () => {
     expect(response.body).toEqual([mockTrack1])
     expect(mockGetSchema).toHaveBeenCalled()
   })
-  test('invalid sid', async () => {
+  test('sid not alphanum', async () => {
     mockGetSchema.mockResolvedValue(null)
     await request(app)
       .get('/api/tracks/byyear/2021/sid/2-3')
       .expect(401)
-    expect(mockGetSchema).toHaveBeenCalled()
   })
 
   test('wrong year format', async () => {
@@ -49,3 +54,34 @@ describe('tracks - byYear', () => {
       .expect(400)
   })
 })
+
+describe('Update track byid', () => {
+  let mockPool
+  beforeEach(() => {
+    mockPool = new Pool()
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  test('simple', async () => {
+    mockPool.query.mockResolvedValue({ rowCount: 1 })
+    mockGetSchema.mockResolvedValue('myschema')
+
+    await request(app)
+      .put('/api/tracks/byid/88/sid/abcsid')
+      .send(
+        {
+          data: { name: 'newname' },
+          updateAttributes: ['name']
+        })
+      .expect(200)
+  })
+})
+
+// describe('Delete track byid', () => {
+//   test('simple', async () => {
+//     await request(app)
+//       .delete('/api/tracks/byid/88/sid/abcsid')
+//       .expect(200)
+//   })
+// })

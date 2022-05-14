8846553917023277c58384ace934670799ddfdf0
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-05-15 00:33:45 +0200

    test for EditableText

---
+++ client/tests/unit/EditableText.spec.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
+import EditableText from '@/components/EditableText.vue'
+
+describe('EditableText', () => {
+  test('Simple', async () => {
+    const mockUpdateFunc = jest.fn()
+    render(
+      EditableText, {
+        props: {
+          updateFunction: mockUpdateFunc,
+          initialtext: 'hello text'
+        }
+      })
+    const editableField = await screen.findByText('hello text')
+    await userEvent.click(editableField)
+    await userEvent.keyboard('{selectall}newvalue{Enter}')
+    expect(mockUpdateFunc.mock.calls.length).toBe(1)
+    expect(mockUpdateFunc.mock.calls[0][0]).toEqual('newvalue')
+  })
+})

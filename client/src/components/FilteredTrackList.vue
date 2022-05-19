192aa490fc170c464518c6f3b720c92d32f85204
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-05-19 22:37:07 +0200

    select and highlight

--- client/src/components/FilteredTrackList.vue
+++ client/src/components/FilteredTrackList.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="p-2">
       <b-card
+        ref="testref"
         no-body
         :header="'List of Tracks - '+trackLoadStatus"
         class="fs-2"
@@ -12,9 +13,11 @@
           <b-list-group-item
             v-for="track in loadedTracksSorted"
             :key="track.id"
+            :ref="'track_'+track.id"
+            :active="itemActiveStatus(track.id)"
           >
-            <span class="text-body">{{ track.name }}, </span>
-            <span class="text-secondary">{{ (track.distance() / 1000).toFixed(0) }} km, {{ track.localeDateShort() }}</span>
+            <span>{{ track.name }}, </span>
+            <span>{{ (track.distance() / 1000).toFixed(0) }} km, {{ track.localeDateShort() }}</span>
           </b-list-group-item>
         </b-list-group>
       </b-card>
@@ -32,6 +35,11 @@ export default {
     BListGroupItem,
     BCard
   },
+  data: function () {
+    return {
+      activeItems: [45]
+    }
+  },
   computed: {
     ...mapState([
       'loadedTracks',
@@ -48,12 +56,22 @@ export default {
       (state) => {
         return this.$store.state.selectedTrack
       },
-      (newValue, oldValue) => {
-        if (newValue) {
-          console.log('in tracklist', newValue)
+      (trackId) => {
+        if (trackId) {
+          this.activeItems = [trackId]
+          const itemRef = `track_${trackId}`
+          this.$refs[itemRef][0].scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+          })
         }
       }
     )
+  },
+  methods: {
+    itemActiveStatus (trackId) {
+      return (this.activeItems.includes(trackId))
+    }
   }
 }
 </script>

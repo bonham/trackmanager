9f701feb89e61889d5dfac06c5f08259a17ecb5d
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-02-19 22:06:04 +0100

    skipping test

--- client/tests/unit/selectTracksHighLevel.spec.js
+++ client/tests/unit/selectTracksHighLevel.spec.js
@@ -12,9 +12,8 @@ describe('SelectTracksPage - DOM testing', () => {
     vi.stubGlobal('fetch', mockFetch)
   })
 
-  test('Load Tracks of 2021', async () => {
-    // fetch.mockResponse(responseMockFunction)
-
+  // does not work because of https://github.com/testing-library/vue-testing-library/issues/298
+  test.skip('Load Tracks of 2021', async () => {
     const storeInstance = createStore(store)
     const rresult = render(SelectTracksPage, {
       props: { sid: 'abcd1234' },
@@ -23,19 +22,11 @@ describe('SelectTracksPage - DOM testing', () => {
       }
     })
 
-    // note for next time: problem: component is not rendering buttons. Is it a rendering or ractivity problem, or is the data in the component not correct
-    // look at console logs in SelectTracksPage - looks like mounted is coming before created ????? Need to do basic tests https://testing-library.com/docs/vue-testing-library/examples
-
     await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(2))
     console.log('Our calls::::', mockFetch.calls)
 
     const button = await rresult.findByText('2021')
     await fireEvent.click(button)
     await rresult.findByText('Saupferchweg,')
-  })
-
-  test.skip('Simple fetch', async () => {
-    const r = await fetch('/')
-    expect(r.status).toBe(200)
   })
 })

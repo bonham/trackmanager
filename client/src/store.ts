5227a28bb8797a1077e2505fad63c2e0c790eddf
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-03-30 23:54:31 +0200

    add comments

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,8 +1,8 @@
 import _ from 'lodash'
 import type { GetterTree, MutationTree, ActionTree } from "vuex"
-import type { Track, TrackPropertiesOptional } from '@/lib/Track' 
+import type { Track, TrackPropertiesOptional } from '@/lib/Track'
 
-type SelectionObject = { selected: number[] , deselected: number[] }
+type SelectionObject = { selected: number[], deselected: number[] }
 const EMPTY_SELECTION_OBJ: SelectionObject = { selected: [], deselected: [] }
 
 class State {
@@ -12,11 +12,13 @@ class State {
   resizeMap: boolean = false
   redrawTracksOnMap: boolean = false
   selectionForList: SelectionObject | null = EMPTY_SELECTION_OBJ
-  selectionForMap: SelectionObject | null = EMPTY_SELECTION_OBJ  
+  // The following field is a 'command' field. Setting this field should trigger some actions in map
+  // after action in map is completed, the 'command' field is cleared
+  selectionForMap: SelectionObject | null = EMPTY_SELECTION_OBJ
 }
 
 const mutations = <MutationTree<State>>{
-  setLoadedTracks (state, trackList: Track[]) {
+  setLoadedTracks(state, trackList: Track[]) {
     state.tracksById = {}
     state.loadedTracks = trackList
     trackList.forEach(function (el) {
@@ -24,52 +26,52 @@ const mutations = <MutationTree<State>>{
       state.tracksById[id] = el
     })
   },
-  modifyTrack (state, { id, props }: { id: number, props: TrackPropertiesOptional }) {
+  modifyTrack(state, { id, props }: { id: number, props: TrackPropertiesOptional }) {
     const trackToModify = state.tracksById[id]
     _.assign(trackToModify, props)
   },
   // for track metadata only
-  setTrackLoadStatus (state, status: number) {
+  setTrackLoadStatus(state, status: number) {
     state.trackLoadStatus = status
   },
   // used as events to indicate that the map needs a resize
-  resizeMapFlag (state) {
+  resizeMapFlag(state) {
     state.resizeMap = true
   },
-  resizeMapClear (state) {
+  resizeMapClear(state) {
     state.resizeMap = false
   },
   // used as event to indicate that the map needs redraw layers
-  redrawTracksOnMapFlag (state, status: boolean) {
+  redrawTracksOnMapFlag(state, status: boolean) {
     state.redrawTracksOnMap = status
   },
-  updateSelectionForList (state, selectChanges: SelectionObject) {
+  updateSelectionForList(state, selectChanges: SelectionObject) {
     state.selectionForList = selectChanges
   },
-  updateSelectionForMap (state, selectChanges: SelectionObject) {
+  updateSelectionForMap(state, selectChanges: SelectionObject) {
     state.selectionForMap = selectChanges
   },
-  clearSelectionForList (state) {
+  clearSelectionForList(state) {
     state.selectionForList = null
   },
-  clearSelectionForMap (state) {
+  clearSelectionForMap(state) {
     state.selectionForMap = null
   }
 };
 
-export interface RootState {}
+export interface RootState { }
 type TrackArray = Track[]
 type LoadFunctionType = () => Promise<TrackArray>
 
 const actions = <ActionTree<State, RootState>>{
 
-  async loadTracks ({ commit }, loadFunction: LoadFunctionType ) {
+  async loadTracks({ commit }, loadFunction: LoadFunctionType) {
     commit('setTrackLoadStatus', 1)
     const trackList = await loadFunction() // [ Track1, Track2, Track3 ] oder auch [ { id: 403, name: "Hammerau", .}, { id: x, ... }]
     commit('setLoadedTracks', trackList)
     commit('setTrackLoadStatus', 2)
   },
-  async loadTracksAndRedraw ({ commit, dispatch }, loadFunction: LoadFunctionType) {
+  async loadTracksAndRedraw({ commit, dispatch }, loadFunction: LoadFunctionType) {
     await commit('updateSelectionForList', EMPTY_SELECTION_OBJ)
     await commit('updateSelectionForMap', EMPTY_SELECTION_OBJ)
     await dispatch('loadTracks', loadFunction)
@@ -78,7 +80,7 @@ const actions = <ActionTree<State, RootState>>{
 
 }
 
-const getters = <GetterTree<State, RootState>> {
+const getters = <GetterTree<State, RootState>>{
   getLoadedTrackIds: state => {
     return _.map(state.loadedTracks, (x) => { return x.id })
   },

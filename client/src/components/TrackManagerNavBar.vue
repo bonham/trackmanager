c8d4580cb0e5d2b4c10b4636a370129f5bdef90f
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-07-10 22:27:40 +0200

    navbar without dropdown - but raw bootstrap

--- client/src/components/TrackManagerNavBar.vue
+++ client/src/components/TrackManagerNavBar.vue
@@ -1,55 +1,46 @@
 <!-- eslint-disable vue/first-attribute-linebreak -->
 <template>
   <div>
-    <b-navbar class="shadow-sm" toggleable="sm">
-      <b-navbar-brand href="#">Trackmanager</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="my-2 my-md-0 mr-md-3">
-          <b-nav-item-dropdown id="dropdown-1" text="Filter" toggle-class="nav-link-custom" right class="p-2">
-            <b-dropdown-item :to="navPath('/select_tracks')" class="p-2">Year</b-dropdown-item>
-            <b-dropdown-item class="p-2" disabled>Text</b-dropdown-item>
-            <b-dropdown-item class="p-2" disabled>Area</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown id="dropdown-2" text="List" toggle-class="nav-link-custom" right class="p-2">
-            <b-nav-item :to="navPath('/toverview')" class="p-2">
-              Sections
-            </b-nav-item>
-            <b-nav-item :to="navPath('/tracklist')" class="p-2">
-              Flat
-            </b-nav-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown id="dropdown-3" text="Maintain" toggle-class="nav-link-custom" right class="p-2">
-            <b-nav-item :to="navPath('/track_multi_edit')" class="p-2 ">
+    <nav class="navbar navbar-expand-sm shadow-sm bg-white border-bottom  ">
+      <div class="container-fluid">
+        <!-- <a class="navbar-brand" to="#">TrckM</a> -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
+          <div class="navbar-nav">
+            <router-link class="nav-link m-2" :to="navPath('/select_tracks')">
+              Map
+            </router-link>
+            <router-link class="nav-link m-2" :to="navPath('/tracklist')">
+              Tracks
+            </router-link>
+            <router-link class="nav-link m-2" :to="navPath('/toverview')">
+              By Year
+            </router-link>
+            <router-link class="nav-link m-2" :to="navPath('/track_multi_edit')">
               Edit
-            </b-nav-item>
-            <b-nav-item :to="navPath('/upload')" class="p-2 ">
+            </router-link>
+            <router-link class="nav-link m-2" :to="navPath('/upload')">
               Upload
-            </b-nav-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script lang="ts">
-import { BNavbar, BNavbarNav, BNavbarToggle, BNavbarBrand, BCollapse, BNavItem, BNavItemDropdown, BDropdownItem } from 'bootstrap-vue-next'
+import { RouterLink } from 'vue-router';
 
 export default {
   name: 'TrackManagerNavBar',
   components: {
-    BNavbar,
-    BNavbarNav,
-    BNavbarToggle,
-    BNavbarBrand,
-    BCollapse,
-    BNavItem,
-    BNavItemDropdown,
-    BDropdownItem
+    RouterLink
   },
+
   props: {
     sid: {
       type: String,
@@ -68,10 +59,13 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+nav {
+
+  a {
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>

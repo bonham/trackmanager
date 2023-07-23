a3e61b20893be171f23a347cf9610d65aa181c34
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-07-23 11:05:46 +0200

    fix linting of vitest globals

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:vitest-globals/recommended'
   ],
   parser: "vue-eslint-parser",
   parserOptions: {
@@ -16,5 +17,8 @@ module.exports = {
       // Script parser for `<script lang="ts">`
       "ts": "@typescript-eslint/parser"
     }
+  },
+  "env": {
+    "vitest-globals/env": true
   }
 }

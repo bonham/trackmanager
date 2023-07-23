dc7e496f06fa8d73a735f80768f6b0f420f04e81
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-07-23 22:46:23 +0200

    options to composition

--- client/src/components/EditableText.vue
+++ client/src/components/EditableText.vue
@@ -5,95 +5,85 @@
       {{ valueOrEmptyPlaceholder }}
     </div>
     <div v-show="editing">
-      <b-form-textarea
-v-if="textarea" ref="inputref" v-model="value" rows="2" max-rows="20" type="textarea"
+      <b-form-textarea v-if="textarea" ref="inputref" v-model="value" rows="2" max-rows="20" type="textarea"
         class="form-control overflow-hidden" @change="processValueChange" @blur="processBlur"
         @keydown.enter="processEnter" />
-      <b-form-input
-v-else ref="inputref" v-model="value" type="text" @blur="processBlur" @change="processValueChange"
+      <b-form-input v-else ref="inputref" v-model="value" type="text" @blur="processBlur" @change="processValueChange"
         @keydown.enter="processEnter" />
     </div>
   </div>
 </template>
-<script lang="ts">
-import {
-  BFormTextarea, BFormInput
-} from 'bootstrap-vue-next'
-export default {
-  components: {
-    BFormTextarea,
-    BFormInput
-  },
-  props:
-  {
-    initialtext: {
-      type: String,
-      default: '',
-      required: false
-    },
-    updateFunction: {
-      type: Function,
-      default: function () { },
-      required: false
-    },
-    textarea: {
-      type: Boolean,
-      default: false,
-      required: false
-    }
-  },
-  emits: {
-    blur
-  },
-  data() {
-    return {
-      editing: false,
-      value: ''
-    }
-  },
-  computed: {
-    valueOrEmptyPlaceholder() {
-      if (this.valueIsEmptyOrWhitespace) {
-        return 'No Name'
-      } else {
-        return this.value
-      }
-    },
-    valueIsEmptyOrWhitespace() {
-      const isEmptyString = (this.value === '')
-      const wsRegex = /^\s+$/
-      const isWhitespace = wsRegex.test(this.value)
-      return (isEmptyString || isWhitespace)
-    }
-  },
-  created() {
-    this.value = this.initialtext
-  },
-  methods: {
-    makeEditable: function () {
-      this.editing = true
-      this.$nextTick(() => {
-        (this.$refs.inputref as any).focus()
-      })
-    },
 
-    processValueChange(value: string) {
-      const inputValue = value
-      console.log('change', inputValue)
-      this.updateFunction(inputValue)
-    },
-    processEnter(event: KeyboardEvent) {
-      const value = (event.target as HTMLInputElement).value
-      const valueNoWhiteSpace = value.trim()
-      this.processValueChange(valueNoWhiteSpace)
-      this.editing = false
-    },
-    processBlur() {
-      this.editing = false
-    }
+<script setup lang="ts">
+import {
+  BFormTextarea,
+  BFormInput
+} from 'bootstrap-vue-next'
+
+import { ref, computed, nextTick } from 'vue'
+
+const props = defineProps({
+  initialtext: {
+    type: String,
+    default: '',
+    required: false
+  },
+  updateFunction: {
+    type: Function,
+    default: function () { },
+    required: false
+  },
+  textarea: {
+    type: Boolean,
+    default: false,
+    required: false
   }
+})
+
+const editing = ref(false)
+const value = ref(props.initialtext)
+const inputref = ref(null)
+
+function makeEditable() {
+  editing.value = true
+
+  nextTick(() => {
+    (inputref.value as any).focus()
+  })
 }
+
+function processValueChange(value: string) {
+  const inputValue = value
+  console.log('change', inputValue)
+  props.updateFunction(inputValue)
+}
+
+function processEnter(event: KeyboardEvent) {
+  const value = (event.target as HTMLInputElement).value
+  const valueNoWhiteSpace = value.trim()
+  processValueChange(valueNoWhiteSpace)
+  editing.value = false
+}
+function processBlur() {
+  editing.value = false
+}
+
+const valueOrEmptyPlaceholder = computed(() => {
+  if (valueIsEmptyOrWhitespace.value) {
+    return 'No Name'
+  } else {
+    return value.value
+  }
+})
+const valueIsEmptyOrWhitespace = computed(() => {
+  const isEmptyString = (value.value === '')
+  const wsRegex = /^\s+$/
+  const isWhitespace = wsRegex.test(value.value)
+  return (isEmptyString || isWhitespace)
+})
+
 </script>
+
 <style scoped>
 .editable-empty {
   opacity: 0.5;

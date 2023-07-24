3d5f5b8a6dfec38463595d39a1e4a8be15f877e5
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-07-24 23:11:42 +0200

    replace runtime with ts-node

---
+++ server/src/bin/copyconfig.ts
@@ -0,0 +1,20 @@
+const { copyFile } = require('fs/promises');
+const { constants } = require('fs');
+
+async function main() {
+  try {
+    await copyFile(
+      'config_dist.js',
+      'config.js',
+      constants.COPYFILE_EXCL,
+    );
+    console.log('config_dist.js was copied to config.js');
+  } catch (e) {
+    if ((e as any).code === 'EEXIST') {
+      console.log('config.js already exists');
+      return;
+    }
+    console.log('The file could not be copied', e);
+  }
+}
+main();

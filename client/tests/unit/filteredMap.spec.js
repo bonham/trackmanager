275490deaeb9c4f5a99b775aa1c874e66f171ac1
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-09-18 21:26:18 +0200

    migrate test to pinia

--- client/tests/unit/filteredMap.spec.js
+++ client/tests/unit/filteredMap.spec.js
@@ -1,11 +1,10 @@
 import { render } from '@testing-library/vue'
-import { mount } from '@vue/test-utils'
+import { mount, DOMWrapper } from '@vue/test-utils'
 
 import FilteredMap from '@/components/FilteredMap.vue'
 import { ManagedMap } from '@/lib/mapservices/ManagedMap'
 
-import { createStore } from 'vuex'
-import { store } from '../../src/store'
+import { createTestingPinia } from '@pinia/testing'
 import { vi, test, beforeEach, describe, expect } from 'vitest'
 import ResizeObserverMock from './__mocks__/ResizeObserver'
 
@@ -14,23 +13,23 @@ describe('Basic store test with FilteredMap', () => {
     vi.stubGlobal('ResizeObserver', ResizeObserverMock)
   })
   test('Trivial mount', async () => {
-    const storeInstance = createStore(store)
     render(FilteredMap, {
       global: {
-        plugins: [storeInstance]
+        plugins: [createTestingPinia]
       }
     })
     // it is not possible to test anything visible here as it contains of <div>  only
   })
 
   test('Low level', () => {
-    const storeInstance = createStore(store)
     const wrapper = mount(FilteredMap, {
       global: {
-        plugins: [storeInstance]
+        plugins: [createTestingPinia]
       }
     })
-    expect(wrapper.vm.shouldBeVisibleIds).toEqual([])
+    const mapdiv = wrapper.find('[id="mapdiv"]')
+    expect(mapdiv).toBeInstanceOf(DOMWrapper)
+    expect(mapdiv.element).toBeInstanceOf(HTMLDivElement)
     expect(wrapper.vm.mmap).toBeInstanceOf(ManagedMap)
   })
 })

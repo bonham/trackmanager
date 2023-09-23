1db6fd333d32c1bbfafc6b7d148c005b25b63154
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-09-24 00:29:30 +0200

    composition api

--- client/src/views/TrackMultiEditPage.vue
+++ client/src/views/TrackMultiEditPage.vue
@@ -21,9 +21,8 @@
         </div>
         <div v-else>
 
-          <editable-text
-:textarea="false" :initialtext="data.value"
-            :update-function="(value: string) => processUpdate(data.item.id, value)" />
+          <editable-text :textarea="false" :initialtext="stringOrEmpty(data.value)"
+            :update-function="(value: string) => processUpdate(numberOrNegative1(data.item.id), value)" />
         </div>
       </template>
       <template #cell(cbutton)="row">
@@ -40,27 +39,36 @@
   </b-container>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import {
   BTable, BButton,
   BContainer, BSpinner
 
 } from 'bootstrap-vue-next'
+import type { TableItem } from 'bootstrap-vue-next'
 import { getAllTracks, updateTrack, updateTrackById, deleteTrack } from '@/lib/trackServices'
 import type { Track } from '@/lib/Track'
 import TrackManagerNavBar from '@/components/TrackManagerNavBar.vue'
 import EditableText from '@/components/EditableText.vue'
+import { ref } from 'vue'
 
-type TableItem = {
-  id: number
-  name: string
-  src: string
-  length: string
-  time: string
-  loading: boolean
+function isString(value: unknown): value is string {
+  return typeof value === 'string';
 }
 
-const trackTableFields = [
+function stringOrEmpty(u: unknown) {
+  return isString(u) ? u : ''
+}
+
+function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
+}
+
+function numberOrNegative1(u: unknown) {
+  return isNumber(u) ? u : -1
+}
+
+const trackTableFields = ref([
   {
     key: 'id',
     label: 'Id',
@@ -99,113 +107,97 @@ const trackTableFields = [
     label: 'Delete',
     tdClass: 'align-middle'
   }
-]
-export default {
-  name: 'TrackMultiEdit',
-  components: {
-    BTable,
-    BButton,
-    BContainer,
-    BSpinner,
-    TrackManagerNavBar,
-    EditableText
-  },
-  props: {
-    sid: {
-      type: String,
-      default: ''
-    }
-  },
-  data() {
-    return {
-      trackTableFields,
-      tableItems: [] as TableItem[],
-      tracksByTrackId: {} as { [index: number]: Track },
-      transProps: {
-        // Transition name
-        name: 'flip-list'
-      },
-      loading: false
-    }
-  },
-  created: async function () {
-    // load data into store
-    await this.loadTracks()
-  },
-  methods: {
+])
 
-    loadTracks: async function () {
-      this.loading = true
-      const tracks = await getAllTracks(this.sid)
-      tracks.sort(
-        (a, b) => {
-          if (!a.time) return -1
-          if (!b.time) return 1
-          return a.time < b.time ? -1 : 1
-        }
-      )
-      tracks.forEach((t) => {
-        // sort by track id
-        this.tracksByTrackId[t.id] = t
-
-        const item = {} as TableItem
-        item.id = t.id
-        item.name = t.name
-        item.src = t.src
-        item.length = (t.distance() / 1000).toFixed(0)
-        item.time = t.localeDateShort()
-        item.loading = false
-
-        this.tableItems.push(item)
-      })
-      this.loading = false
-    },
-    cleanUpText: async function (item: TableItem) {
-      const id = item.id
-      item.loading = true
-      let convertedName = item.src
-      const datePattern = /\d{8}/
-      const match = convertedName.match(datePattern)
-      if (match) {
-        // const date = match[0]
-        convertedName = convertedName.replace(datePattern, '')
-      }
-      convertedName = convertedName.replace(/[ \-_]+/g, ' ') // convert to space
-      convertedName = convertedName.replace(/\.gpx$/i, '') // strip file suffix
-      convertedName = convertedName.trim() // Trim space at begin or end
-
-      // Update track in store and on server
-      const track = this.tracksByTrackId[id]
-      // Update property in track item
-      track.name = convertedName
-
-      // Perform update in backend
-      const updateAttributes = ['name']
-      await updateTrack(track, updateAttributes, this.sid)
-      item.name = convertedName
-      item.loading = false
-    },
-    cleanAll: function () {
-      this.tableItems.forEach(item => {
-        this.cleanUpText(item)
-      })
-    },
-    deleteTrackFromTable: async function (item: TableItem) {
-      const success = await deleteTrack(item.id, this.sid)
-      if (success) {
-        delete this.tracksByTrackId[item.id]
-        // delete element
-        const idx = this.tableItems.findIndex((e) => e.id === item.id)
-        this.tableItems.splice(idx, 1)
-      }
-    },
-    processUpdate(trackId: number, value: string) {
-      console.log('in upper component:', trackId, value)
-      updateTrackById(trackId, { name: value }, this.sid)
-    }
+const props = defineProps({
+  sid: {
+    type: String,
+    default: ''
   }
+})
 
+const tableItems = ref<TableItem[]>([])
+const tracksByTrackId = ref<{ [index: number]: Track }>({})
+const loading = ref(false)
+
+
+loadTracks().catch((e) => { console.error(e) })
+
+
+async function loadTracks() {
+  loading.value = true
+  const tracks = await getAllTracks(props.sid)
+  tracks.sort(
+    (a, b) => {
+      if (!a.time) return -1
+      if (!b.time) return 1
+      return a.time < b.time ? -1 : 1
+    }
+  )
+  tracks.forEach((t) => {
+    // sort by track id
+    tracksByTrackId.value[t.id] = t
+
+    const item = {} as TableItem
+    item.id = t.id
+    item.name = t.name || ""
+    item.src = t.src || ""
+    item.length = (t.distance() / 1000).toFixed(0)
+    item.time = t.localeDateShort()
+    item.loading = false
+
+    tableItems.value.push(item)
+  })
+  loading.value = false
 }
+async function cleanUpText(item: TableItem) {
+  if (!isNumber(item.id)) throw Error("item.id not number")
+  const id = item.id
+  item.loading = true
+  let convertedName = stringOrEmpty(item.src)
+  const datePattern = /\d{8}/
+  const match = convertedName.match(datePattern)
+  if (match) {
+    // const date = match[0]
+    convertedName = convertedName.replace(datePattern, '')
+  }
+  convertedName = convertedName.replace(/[ \-_]+/g, ' ') // convert to space
+  convertedName = convertedName.replace(/\.gpx$/i, '') // strip file suffix
+  convertedName = convertedName.trim() // Trim space at begin or end
+
+  const track = tracksByTrackId.value[id]
+  // Update property in track item
+  track.name = convertedName
+
+  // Perform update in backend
+  const updateAttributes = ['name']
+  await updateTrack(track, updateAttributes, props.sid)
+  item.name = convertedName
+  item.loading = false
+}
+function cleanAll() {
+  tableItems.value.forEach(item => {
+    cleanUpText(item)
+  })
+}
+async function deleteTrackFromTable(item: TableItem) {
+  if (!isNumber(item.id)) throw Error("item.id not number")
+  const success = await deleteTrack(item.id, props.sid)
+  if (success) {
+    delete tracksByTrackId.value[item.id]
+    // delete element
+    const idx = tableItems.value.findIndex((e) => e.id === item.id)
+    tableItems.value.splice(idx, 1)
+  }
+}
+
+function processUpdate(trackId: number, value: string) {
+  console.log('in upper component:', trackId, value)
+  updateTrackById(trackId, { name: value }, props.sid)
+}
+
+
+
 </script>
 <style>
 .flip-list-move {

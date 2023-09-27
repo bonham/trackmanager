cd54aaf5b7ab4de3decfa159f51c157ec345dc79
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-09-27 21:38:39 +0200

    suppress warnings

--- client/tests/unit/UploadPage.spec.js
+++ client/tests/unit/UploadPage.spec.js
@@ -3,12 +3,31 @@ import userEvent from '@testing-library/user-event'
 import UploadPage from '@/views/UploadPage.vue'
 import { vi } from 'vitest'
 import { Response } from 'cross-fetch'
+import { beforeEach } from 'vitest'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+
 
 describe('UploadPage', () => {
+  let router
+  beforeEach(() => {
+    router = createRouter({
+      history: createWebHistory(),
+      routes: [], // provide your routes here
+    })
+
+  })
   test('UploadSuccess', async () => {
     vi.stubGlobal('fetch', vi.fn(() => new Response('{"message":"ok"}')))
     const user = userEvent.setup()
-    const { getByLabelText } = render(UploadPage)
+    const { getByLabelText } = render(UploadPage,
+      {
+        global: {
+          plugins: [router]
+        },
+        stubs: ['router-link']
+      })
     const input = getByLabelText(/Drop files/i)
     const file = new File(['hello'], 'hello.gpx', { type: 'text/xml' })
 
@@ -22,7 +41,13 @@ describe('UploadPage', () => {
     vi.stubGlobal('fetch', vi.fn(() => response))
 
     const user = userEvent.setup()
-    const { getByLabelText, findByText } = render(UploadPage)
+    const { getByLabelText, findByText } = render(UploadPage,
+      {
+        global: {
+          plugins: [router]
+        },
+        stubs: ['router-link']
+      })
     const input = getByLabelText(/Drop files/i)
     const file = new File(['hello'], 'hello.gpx', { type: 'text/xml' })
 

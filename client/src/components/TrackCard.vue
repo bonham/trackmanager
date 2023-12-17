a6c7e9b387b44b8b0e1a64a776236b8d92a131ca
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-12-17 23:38:38 +0100

    make whole card clickable

--- client/src/components/TrackCard.vue
+++ client/src/components/TrackCard.vue
@@ -1,64 +1,56 @@
 <template>
-  <b-card class="my-2">
-    <b-card-text>
-      <b-row class="align-items-center">
-        <b-col cols="9">
-          <h4>
-            {{ track.name || track.src }}
-          </h4>
-          <div>
-            {{ track.monthAndDay() }}
-            / {{ (track.distance() / 1000).toFixed(2) }} km
-            / {{ track.ascent ? Math.round(track.ascent) : "-" }} m
-            / {{ track.timeLengthFormatted() }} h
-            / {{ track.timelength }} s
-            / {{ track.timelength ? Math.round((3.6 * track.distance() / track.timelength) * 10) / 10 : "-" }} km/h avg.
-          </div>
-        </b-col>
-        <b-col cols="3" class="d-flex align-items-center justify-content-end">
-          <b-link :to="'/track/' + track.id + '/sid/' + sid" :aria-label="ariaLinkLabel">
+  <b-link :to="'/track/' + track.id + '/sid/' + sid" :aria-label="ariaLinkLabel">
+    <b-card class="my-2">
+      <b-card-text>
+        <b-row class="align-items-center">
+          <b-col cols="9">
+            <h4>
+              {{ track.name || track.src }}
+            </h4>
+            <div class="text-decoration-none">
+              {{ track.monthAndDay() }}
+              / {{ (track.distance() / 1000).toFixed(2) }} km
+              / {{ track.ascent ? Math.round(track.ascent) : "-" }} m
+              / {{ track.timeLengthFormatted() }} h
+              / {{ track.timelength }} s
+              / {{ track.timelength ? Math.round((3.6 * track.distance() / track.timelength) * 10) / 10 : "-" }} km/h
+              avg.
+            </div>
+          </b-col>
+          <b-col cols="3" class="d-flex align-items-center justify-content-end">
             <i-bi-chevron-right />
-          </b-link>
-        </b-col>
-      </b-row>
-    </b-card-text>
-  </b-card>
+          </b-col>
+        </b-row>
+      </b-card-text>
+    </b-card>
+  </b-link>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 
 import { Track } from '@/lib/Track'
 import {
   BCard, BCardText,
   BRow, BCol, BLink
 } from 'bootstrap-vue-next'
+import { computed } from 'vue'
 
-export default {
-  name: 'TrackCard',
-  components: {
-    BCard,
-    BCardText,
-    BRow,
-    BCol,
-    BLink
+const props = defineProps({
+  track: {
+    type: Track,
+    default: null
   },
-  props: {
-    track: {
-      type: Track,
-      default: null
-    },
-    sid: {
-      type: String,
-      default: ''
-    }
-
-  },
-  computed: {
-    ariaLinkLabel() {
-      return `link-to-track-${this.track.id}`
-    }
+  sid: {
+    type: String,
+    default: ''
   }
-}
+
+})
+
+const ariaLinkLabel = computed(() => {
+  return `link-to-track-${props.track.id}`
+})
+
 </script>
 <style scoped>
 .card-icon {

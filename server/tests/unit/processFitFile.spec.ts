d2a31ea556c731eec2ee72c322ecc2dfeaf0a089
Art Furrer <2823246+bonham@users.noreply.github.com>
2024-05-15 23:11:07 +0200

    fix test

--- server/tests/unit/processFitFile.spec.ts
+++ server/tests/unit/processFitFile.spec.ts
@@ -1,10 +1,16 @@
 import { jest, test } from '@jest/globals';
 import { readFileSync } from 'node:fs';
-import { Track2DbWriter } from '../../src/lib/Track2DbWriter.js';
 import { processFitFile } from '../../src/lib/processFitFile.js';
 
+import { Track2DbWriter } from '../../src/lib/Track2DbWriter.js';
+jest.mock('../../src/lib/Track2DbWriter.js')
+
+type DbOpts = Parameters<InstanceType<typeof Track2DbWriter>['init']>[0];
+
+const mockInit = jest.fn<(o: DbOpts) => Promise<void>>();
 const mockWrite = jest.fn<() => Promise<number>>()
 jest.spyOn(Track2DbWriter.prototype, 'write').mockImplementation(mockWrite)
+jest.spyOn(Track2DbWriter.prototype, 'init').mockImplementation(mockInit)
 
 describe('FitFile', () => {
 

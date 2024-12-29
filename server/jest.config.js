addd6cebf02fca61ee9e832501eba72031785ad8
Art Furrer <2823246+bonham@users.noreply.github.com>
2024-12-29 19:32:41 +0100

    use preset helper functions

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,25 +1,20 @@
-import preset from 'ts-jest/presets/index.js'
+import { createJsWithTsEsmPreset } from 'ts-jest'
+
+const presetConfig = createJsWithTsEsmPreset({
+  tsconfig: 'tsconfig-jest.json'
+})
 
 const jestConfig = {
-  ...preset.defaultsESM,
+  ...presetConfig,
   testEnvironment: "node",
   verbose: false,
   rootDir: ".",
-  extensionsToTreatAsEsm: [".ts"],
   testPathIgnorePatterns: ['/dist/'],
   transformIgnorePatterns: [
     '/node_modules/(?!(@garmin/fitsdk))',
   ],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  transform: {
-    '^.+\\.[tj]sx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
   },
 }
 export default jestConfig

a1c0b29c75a3d8be2fa878747c0eb166d3d40388
Art Furrer <2823246+bonham@users.noreply.github.com>
2025-08-02 21:05:21 +0200

    Rewrite bcollapse to modern

--- client/src/components/TrackSection.vue
+++ client/src/components/TrackSection.vue
@@ -1,13 +1,13 @@
 <!-- eslint-disable vue/first-attribute-linebreak -->
 <template>
   <div>
-    <b-card bg-variant="light" class="my-2" @click="toggleMemberVisibility">
+    <b-card bg-variant="light" class="my-2" @click="toggle">
       <b-card-text>
         <b-row class="align-items-center">
           <b-col cols="9" class="d-flex flex-row align-items-center">
-            <b-button :class="expanded ? null : 'collapsed'" :aria-expanded="expanded ? 'true' : 'false'"
+            <b-button :class="visible ? null : 'collapsed'" :aria-expanded="visible ? 'true' : 'false'"
               :aria-controls="collapseId">
-              <i-bi-arrow-down-circle-fill v-if="expanded" />
+              <i-bi-arrow-down-circle-fill v-if="visible" />
               <i-bi-arrow-right-circle v-else />
             </b-button>
             <h4 class="mx-2 my-0">
@@ -21,17 +21,15 @@
         </b-row>
       </b-card-text>
     </b-card>
-    <b-collapse :id="collapseId" :visible="expanded" data-testid="testbcollapse">
-      <div v-if="everVisible">
-        <TrackCard v-for="item in myDataList" :key="item.id" :track="item" :sid="sid" />
-      </div>
+    <b-collapse :id="collapseId" ref="myCollapse" v-model="visible" data-testid="testbcollapse" :lazy="true">
+      <TrackCard v-for="item in myDataList" :key="item.id" :track="item" :sid="sid" />
     </b-collapse>
   </div>
 </template>
 
 <script setup lang="ts">
 // import TrackHeader from '@/components/TrackHeader.vue'
-import { toRef, ref, computed } from 'vue'
+import { toRef, ref, computed, defineExpose } from 'vue'
 import TrackCard from '@/components/TrackCard.vue'
 import { TrackCollection } from '@/lib/Track'
 import {
@@ -58,21 +56,33 @@ const props = defineProps({
   }
 })
 
+// Ref to the collapse component
+// This is used to control the collapse programmatically if needed
+const myCollapse = ref<InstanceType<typeof BCollapse> | null>(null)
+
+const show = () => myCollapse.value?.show()
+const hide = () => myCollapse.value?.hide()
+const toggle = () => myCollapse.value?.toggle()
+
+// Expose methods to the parent component
+defineExpose({
+  show,
+  hide,
+  toggle
+})
+
+// needed for v-model binding
+const visible = ref(!props.initiallyCollapsed) // initial state
+
+//??
 const collRef = toRef(props, 'coll')
 const myDataList = computed(() => collRef.value.members())
-const expanded = ref(!props.initiallyCollapsed) // initial state
-const everVisible = ref(!props.initiallyCollapsed) // initial state
 
+// Computed property to generate a unique collapse ID based on the label
 const collapseId = computed(() => {
   const origLabel = props.label.replace(/\s+/, '-').toLowerCase()
   return `toggle-${origLabel}`
 })
 
-function toggleMemberVisibility() {
-  const toBeValue = !(expanded.value)
-  // render if it was not rendered before, but never destroy again
-  everVisible.value = everVisible.value || toBeValue
-  // expand after rendering
-  expanded.value = toBeValue
-}
+
 </script>
